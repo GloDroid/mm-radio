016dca0d3aad5e55c1c7bcdbd9bf88753f860ad5
Roman Stratiienko <r.stratiienko@gmail.com>
2023-03-01 19:59:40 +0200

    mm-radio: Rework incoming SMS handling
    1. Make it non-blocking
    2. Handle corner-cases
    3. Add retries / verbosity to allow further debugging
    
    Should fix blocking of IRadio requests if SMS can't be obtained.
    
    From design point of view solution is still bad, but we don't have
    anything better yet.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- src/radio/radio_messaging.rs
+++ src/radio/radio_messaging.rs
@@ -9,7 +9,7 @@ use crate::{
     mm_zbus::{
         consts::{
             mm_modem_state,
-            mm_sms_state::{self, RECEIVED, RECEIVING},
+            mm_sms_state::{RECEIVED, RECEIVING},
         },
         mm_messaging_proxy::MessagingProxy,
         mm_modem_proxy::ModemProxy,
@@ -32,16 +32,15 @@ use android_hardware_radio_messaging::aidl::android::hardware::radio::messaging:
 };
 
 use async_std::{
-    sync::RwLock,
+    channel::Sender,
+    future::timeout,
+    sync::{Mutex, RwLock},
     task::{block_on, spawn},
 };
 use binder::{BinderFeatures, Strong};
 use futures::{select, FutureExt, StreamExt};
-use log::info;
-use std::{
-    collections::{HashMap, VecDeque},
-    sync::Arc,
-};
+use log::{error, info};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 use zbus::{export::async_trait::async_trait, zvariant::OwnedObjectPath};
 
 #[derive(Default)]
@@ -53,7 +52,9 @@ pub struct RadioMessagingShared {
     modem_proxy: Option<ModemProxy<'static>>,
     messaging_proxy: Option<MessagingProxy<'static>>,
 
-    sms_deliver_queue: VecDeque<OwnedObjectPath>,
+    incoming_sms_list: Vec<OwnedObjectPath>,
+    incoming_sms_process_lock: Arc<Mutex<()>>,
+    incoming_sms_confirmation: Option<Sender<bool>>,
 }
 
 #[derive(Default)]
@@ -74,9 +75,6 @@ impl RadioMessagingShared {
             shared.modem_bound = true;
         }
 
-        /* Query RECEIVED SMS from the Modem Manager (can fail if modem is disabled) */
-        let _ = block_on(Self::query_pending_sms(shared_in));
-
         /* Subscribe for modem enable events to query pending SMS */
         {
             let shared_in_c = shared_in.clone();
@@ -85,6 +83,7 @@ impl RadioMessagingShared {
                     let shared = shared_in_c.read().await;
                     shared.modem_proxy.as_ref().unwrap().clone()
                 };
+                let _state = mproxy.state().await; /* get state here to avoid getting initial state below */
                 let mut st_prop = mproxy.receive_state_changed().await;
                 loop {
                     select! {
@@ -117,12 +116,7 @@ impl RadioMessagingShared {
                             let received = added.args().unwrap().received;
                             if received {
                                 let path = added.args().unwrap().path;
-                                info!("Received new SMS: {}", path);
-                                {
-                                    let mut shared = shared_in_c.write().await;
-                                    shared.sms_deliver_queue.push_back(path.into());
-                                }
-                                RadioMessagingShared::forward_next_sms(&shared_in_c).await;
+                                spawn(Self::handle_message_received(shared_in_c.clone(), path.into()));
                             }
                         }
                         complete => break,
@@ -130,8 +124,6 @@ impl RadioMessagingShared {
                 }
             });
         }
-
-        Self::notify_framework(shared_in);
     }
 
     pub fn unbind(shared_in: &Arc<RwLock<RadioMessagingShared>>) {
@@ -146,53 +138,67 @@ impl RadioMessagingShared {
         let shared = shared_in.read().await;
         let messages = shared.messaging_proxy.as_ref().unwrap().messages().await?;
 
-        let mut received_sms = VecDeque::new();
-
         let conn = shared.modem_proxy.as_ref().unwrap().connection();
         for path in messages {
             let sms_proxy = SmsProxy::builder(conn).path(path.clone())?.build().await?;
             let state = sms_proxy.state().await?;
-            if (state == RECEIVED || state == RECEIVING)
-                && !shared.sms_deliver_queue.contains(&path)
-            {
-                received_sms.push_back(path);
+            if state == RECEIVED || state == RECEIVING {
+                spawn(Self::handle_message_received(shared_in.clone(), path));
             }
-        }
-        drop(shared);
-        {
-            let mut shared = shared_in.write().await;
-            shared.sms_deliver_queue.extend(received_sms);
         }
 
         Ok(())
     }
 
-    async fn forward_next_sms(shared_in: &Arc<RwLock<RadioMessagingShared>>) {
-        let shared = shared_in.read().await;
-        if !shared.modem_bound || shared.response.is_none() || shared.indication.is_none() {
-            return;
+    async fn delete_sms(shared_in: Arc<RwLock<RadioMessagingShared>>, path: OwnedObjectPath) {
+        info!("Deleting SMS message: {path}");
+
+        let result =
+            shared_in.as_ref().read().await.messaging_proxy.as_ref().unwrap().delete(&path).await;
+
+        if let Err(e) = result {
+            info!("Failed to delete SMS message at {path}: {e}");
+        }
+    }
+
+    /* must be a spawned async task */
+    async fn handle_message_received(
+        shared_in: Arc<RwLock<RadioMessagingShared>>,
+        path: OwnedObjectPath,
+    ) -> Result<(), zbus::Error> {
+        /* Avoid duplicates */
+        {
+            let mut shared = shared_in.write().await;
+            if shared.incoming_sms_list.contains(&path) {
+                drop(shared);
+                info!("SMS message '{path}' was already processed");
+                return Ok(());
+            }
+            shared.incoming_sms_list.push(path.clone());
         }
 
-        let path = match shared.sms_deliver_queue.front() {
-            Some(path) => path,
-            None => return,
+        let sms_proxy = {
+            let shared = shared_in.read().await;
+            let conn = shared.modem_proxy.as_ref().unwrap().connection();
+            SmsProxy::builder(conn).path(path.clone()).unwrap().build().await.unwrap()
         };
 
-        let conn = shared.modem_proxy.as_ref().unwrap().connection();
-        let sms_proxy = SmsProxy::builder(conn).path(path).unwrap().build().await.unwrap();
+        info!("New SMS message receiving: {path}");
 
-        // Wait for the SMS to be fully received, limit 30 tries x 1s
-        for _ in 0..30 {
-            let state = sms_proxy.state().await.unwrap();
-            if state == mm_sms_state::RECEIVED {
-                break;
+        loop {
+            match sms_proxy.receive_state_changed().await.next().await.unwrap().get().await? {
+                RECEIVING => continue,
+                RECEIVED => break,
+                s => {
+                    error!("Unknown SMS '{path}' state: {s}");
+                    return Ok(());
+                }
             }
-            async_std::task::sleep(std::time::Duration::from_secs(1)).await;
         }
 
-        let number = sms_proxy.number().await.unwrap();
-        let text = sms_proxy.text().await.unwrap();
-        let timestamp = sms_proxy.timestamp().await.unwrap();
+        let number = sms_proxy.number().await?;
+        let text = sms_proxy.text().await?;
+        let timestamp = sms_proxy.timestamp().await?;
 
         info!("Received message from '{}' at '{}' with text '{}'", number, timestamp, text);
 
@@ -208,12 +214,36 @@ impl RadioMessagingShared {
             pdu_bytes.push(byte);
         }
 
-        let ind = shared.indication.as_ref().unwrap();
-        ind.newSms(RadioIndicationType::UNSOLICITED, &pdu_bytes).unwrap();
-    }
+        let lock = shared_in.read().await.incoming_sms_process_lock.clone();
+        loop {
+            /* Lock, so multiple tasks won't report messages simultaneously */
+            let _lock = lock.lock().await;
+            let mut shared = shared_in.write().await;
+            let ind = shared.indication.clone();
+            if ind.is_none() {
+                drop(shared);
+                // Ugly, so contributions are welcome
+                info!("No IRadio callbacks registered when reporting SMS: {path}, retry in 10 seconds...");
+                async_std::task::sleep(Duration::from_secs(10)).await;
+                continue;
+            }
+            let (sms_processed, wait_sms_processed) = async_std::channel::bounded::<bool>(1);
+            shared.incoming_sms_confirmation = Some(sms_processed);
+            let ind = ind.unwrap();
+            ind.newSms(RadioIndicationType::UNSOLICITED, &pdu_bytes).unwrap();
+            drop(shared);
 
-    fn notify_framework(shared_in: &Arc<RwLock<RadioMessagingShared>>) {
-        block_on(Self::forward_next_sms(shared_in));
+            let fut = wait_sms_processed.into_future();
+            let result = timeout(Duration::from_secs(5), fut).await;
+            if result.is_err() {
+                error!("Timeout accepting incoming SMS: {path}");
+                continue;
+            }
+            break;
+        }
+
+        Self::delete_sms(shared_in, path).await;
+        Ok(())
     }
 }
 
@@ -236,15 +266,11 @@ impl IRadioMessagingAsyncServer for RadioMessaging {
     ) -> binder::Result<()> {
         entry_check!(&self, serial, acknowledgeLastIncomingGsmSmsResponse);
 
-        let sms_path = sharedmut!(&self).sms_deliver_queue.pop_front();
-
-        if let Some(path) = sms_path {
-            let result: Result<(), zbus::Error> = try {
-                let shared = shared!(&self);
-                shared.messaging_proxy.as_ref().unwrap().delete(&path).await?;
-            };
-            result.unwrap_or_else(|e| error!("Failed to delete SMS: {}", e));
-            RadioMessagingShared::forward_next_sms(&self.shared).await;
+        {
+            let shared = shared!(&self);
+            if let Some(confirmation) = &shared.incoming_sms_confirmation {
+                confirmation.send(true).await.unwrap();
+            }
         }
 
         okay!(&self, serial, acknowledgeLastIncomingGsmSmsResponse)
@@ -413,8 +439,6 @@ impl IRadioMessagingAsyncServer for RadioMessaging {
             shared.response = Some(radio_response.clone());
             shared.indication = Some(radio_indication.clone());
         }
-
-        RadioMessagingShared::notify_framework(&self.shared);
 
         Ok(())
     }
