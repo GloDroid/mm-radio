6dfd0b02aa5d598b9e429a7ef0bb1e8d8992e281
Roman Stratiienko <r.stratiienko@gmail.com>
2023-03-11 00:51:32 +0200

    mm-radio: pdu_helpers: Use error propagation instead of unwrap
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- src/utils/sms_deliver_encode.rs
+++ src/utils/sms_deliver_encode.rs
@@ -10,18 +10,24 @@
 use crate::utils::pdu_helpers::address::address_to_pdu;
 use crate::utils::pdu_helpers::time::Timestamp;
 
-pub(crate) fn sms_deliver_encode(address: &str, text: &str, timestamp: &str) -> String {
+use super::error::Error;
+
+pub(crate) fn sms_deliver_encode(
+    address: &str,
+    text: &str,
+    timestamp: &str,
+) -> Result<String, Error> {
     let mut pdu = String::new();
     // SMS-DELIVER (TP-MTI = 0b00) (TP-MMS = 0b0) (TP-SRI = 0b0) (TP-UDHI = 0b0) (TP-RP = 0b0)
     pdu.push_str("00");
     // Address
-    pdu.push_str(&address_to_pdu(address, false).unwrap());
+    pdu.push_str(&address_to_pdu(address, false)?);
     // Protocol identifier
     pdu.push_str("00");
     // Data coding scheme (UCS2)
     pdu.push_str("08");
     // Timestamp
-    pdu.push_str(&Timestamp::from_mm_format(timestamp).to_pdu());
+    pdu.push_str(&Timestamp::from_mm_format(timestamp)?.to_pdu());
     // User data length
     let text_len_bytes = std::cmp::min(text.chars().count() * 2, 254);
     pdu.push_str(&format!("{text_len_bytes:02X}"));
@@ -30,11 +36,12 @@ pub(crate) fn sms_deliver_encode(address: &str, text: &str, timestamp: &str) -> 
         pdu.push_str(&format!("{:04X}", c as u16));
     }
 
-    pdu
+    Ok(pdu)
 }
 
 #[cfg(test)]
 mod tests {
+    #[warn(clippy::unwrap_used, clippy::expect_used)]
     use super::*;
 
     #[test]
@@ -42,7 +49,7 @@ mod tests {
         let address = "diafaan";
         let text = "diafaan.com";
         let timestamp = "2011-01-11 13:25:41+00";
-        let pdu = sms_deliver_encode(address, text, timestamp);
+        let pdu = sms_deliver_encode(address, text, timestamp).unwrap();
         assert_eq!(
             pdu,
             "000ED0E474D81C0EBB0100081110113152140016006400690061006600610061006E002E0063006F006D"
@@ -51,7 +58,7 @@ mod tests {
         let address = "+123456789";
         let text = "Послуга тимчасово недоступна. Спробуйте будь ласка пізніше.";
         let timestamp = "2019-02-15 21:52:19+00";
-        let pdu = sms_deliver_encode(address, text, timestamp);
+        let pdu = sms_deliver_encode(address, text, timestamp).unwrap();
         assert_eq!(pdu, "00099121436587F900089120511225910076041F043E0441043B044304330430002004420438043C044704300441043E0432043E0020043D04350434043E044104420443043F043D0430002E00200421043F0440043E043104430439044204350020043104430434044C0020043B04300441043A04300020043F04560437043D045604480435002E");
     }
 }
